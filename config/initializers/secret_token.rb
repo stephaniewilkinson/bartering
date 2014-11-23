7012915a180b3010ef3a117d1e89db30e9e9c46b
Stephanie Betancourt <what.happens@gmail.com>
2014-11-23 09:22:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bartering::Application.config.secret_token = '6751ec90180ae9ce3ef2436dc9fea18c271e388a40bbeef8a8b9133bf84792dbe55d65882485003b5b58e585ecc6127c11f9c18e4eacfdf574e435efa079dc23'
